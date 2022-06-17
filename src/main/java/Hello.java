5fea5acc3f5cfe3996a596573197539be0704f57
FreeJim <52058772+a1020151695@users.noreply.github.com>
2022-06-17 18:09:39 +0800

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+
+import utils.HelloUtils;
+
+public class Hello {
+    public static void main(String[] args) {
+        HelloUtils utils = new HelloUtils();
+        utils.sayHello();
+    }
+}
---
+++ sub-module1/src/main/java/utils/HelloUtils.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class HelloUtils {
+    public void sayHello(){
+        System.out.println("hello");
+    }
+}
